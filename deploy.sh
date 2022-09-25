aa4de57f5ca28af515e9e407a5f1e48c9b1d45e1
Alexey Martynov <martynov.a@selectel.ru>
2022-09-26 01:00:14 +0300

    removed some &&

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
-sudo apt update && \\
-sudo apt install -y git && \\
+sudo apt update
+sudo apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git && \\
 cd reddit && \\
 bundle install && \\
