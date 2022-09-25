6fe0b2326f21440ea11e54a0c8929f12dcdad21c
Alexey Martynov <martynov.a@selectel.ru>
2022-09-25 23:23:52 +0300

    Bash scripts install_ruby.sh,install_mongodb.sh,deploy.sh have been added

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~
+sudo apt update && \\
+sudo apt install git && \\
+git clone -b monolith https://github.com/express42/reddit.git && \\
+cd reddit && \\
+bundle install && \\
+puma -d && \\
+ps aux | grep puma && \\
+echo  "Open in browser http://reddit-app_ip:9292"
